1858db36fe5570bdf12f3188d4e8c79b8b1a47f2
reiend <145087085+EndreiDarkRose@users.noreply.github.com>
2024-03-22 19:54:36 +0900

    fix input form

--- src/app/call/page.tsx
+++ src/app/call/page.tsx
@@ -4,6 +4,14 @@ import React from "react";
 import styles from "./call.module.scss";
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default function Call() {
   const {
@@ -15,30 +23,43 @@ export default function Call() {
     mode: "onBlur",
   });
 
-  const onSubmitData = (data) => {
-    axios
-      .post(`https://65e7001353d564627a8d9655.mockapi.io/User`, data)
-      .then((response) => {
-        console.log("Успешно отправлено:", response.data);
-      })
-      .catch((error) => {
-        console.error("Ошибка при отправке:", error);
-      });
-    reset();
+  const onSubmitData = async (data) => {
+    try {
+      const response = await axios.post(
+        `https://65e7001353d564627a8d9655.mockapi.io/User`,
+        data
+      );
+      console.log("Успешно отправлено:", response.data);
+      reset();
+    } catch (error) {
+      console.error("Ошибка при отправке:", error);
+    }
   };
+  React.useEffect(() => {
+    const asteriskElement = document.querySelector(
+      ".MuiFormControlLabel-asterisk"
+    );
+    if (asteriskElement) {
+      asteriskElement.remove();
+    }
+  }, []);
+
   return (
     <form className={styles.inputSection} onSubmit={handleSubmit(onSubmitData)}>
-      <input
-        placeholder="Ваше имя"
+      <TextField
+        label="Ваше имя"
+        variant="outlined"
+        sx={{ borderRadius: 12 }}
         {...register("name", {
           required: true,
         })}
       />
       <div>{errors?.name && <p className={styles.message}>Ведите имя</p>}</div>
-      <input
+      <TextField
+        label="Номер телефона"
+        variant="outlined"
         type="number"
         inputMode="numeric"
-        placeholder="Номер телефона"
         {...register("phoneNumber", {
           required: true,
           minLength: 11,
@@ -49,7 +70,19 @@ export default function Call() {
           <p className={styles.message}>Неправильно набран номер</p>
         )}
       </div>
-      <button type="submit">Отправить</button>
+      <FormControlLabel
+        required
+        control={<Checkbox />}
+        label={
+          <Typography variant="body2">
+            Согласен(а) на обработку{" "}
+            <a href="/policy" target="_blank" rel="noopener noreferrer">
+              персональных данных
+            </a>
+          </Typography>
+        }
+      />
+      <Button type="submit">Отправить</Button>
     </form>
   );
 }
